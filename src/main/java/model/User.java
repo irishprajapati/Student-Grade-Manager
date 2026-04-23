f16c600f8ad16be8c531ed878e44df8ab8f6b51e
Erish <erish968@gmail.com>
2026-04-23 16:16:24 +0545

    feat: added the error handling part in model

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,8 +1,96 @@
 package model;
 
+import util.PasswordUtil;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class User {
     private int id;
     private String username;
     private String password;
+    private Role role;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
+    public User(int id, String username, String password, Role role, LocalDateTime createdAt, LocalDateTime updatedAt){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+    public User(String username, String password, Role role){
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUsername(String username) {
+        if (username == null) {
+            throw new IllegalArgumentException("Name cannot be null");
+        }
+        username = username.trim();
+        if (username.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        // normalize multiple spaces → single space
+        username = username.replaceAll("\\s+", " ");
+        if (username.length() < 2 || username.length() > 50) {
+            throw new IllegalArgumentException("Name must be between 2 and 50 characters");
+        }
+        // Unicode-safe (important)
+        if (!username.matches("^[\\p{L} .'-]+$")) {
+            throw new IllegalArgumentException("Name contains invalid characters");
+        }
+        this.username = username;
+    }
+    public void setPassword(String password){
+        if(password == null){
+            throw new IllegalArgumentException("Password cannot be null");
+        }
+        password = password.trim();
+        if(password.isEmpty()){
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+        password = password.replaceAll("\\s+", " ");
+        if(password.length() < 8 || password.length() > 50){
+            throw new IllegalArgumentException("password must be between 8 to 50 words");
+        }
+        if (!password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$")) {
+            throw new IllegalArgumentException("Password must be at least 8 characters long and include uppercase, lowercase, number, and special character");
+        }
+        this.password = PasswordUtil.hashPassword(password);
+    }
+
+    public void setRole(String input) {
+        this.role = Role.fromString(input);
+        this.updatedAt = updatedAt;
+    }
 }
